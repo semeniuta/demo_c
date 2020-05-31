1359d4081432b3c35c17b2df3dc4ff22e1ea3d4f
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2020-05-31 22:16:09 +0200

    Implement socket, bind and close calls in udsock_srv.c

--- udsock_srv.c
+++ udsock_srv.c
@@ -1,5 +1,6 @@
 #include <sys/un.h>
 #include <sys/socket.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -20,10 +21,29 @@ int main() {
 
     struct sockaddr_un addr = init_address("/tmp/myscoket");
 
-    printf("Socket address (size=%ld): %s\n", strlen(addr.sun_path), addr.sun_path);
-
+    printf("Socket address (length %ld): %s\n", strlen(addr.sun_path), addr.sun_path);
     printf("sun_len: %d\n", addr.sun_len);
     printf("size of sun_path: %ld\n", sizeof(addr.sun_path));
+
+    int fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (fd == -1) {
+        printf("Error creating the socket\n");
+        return -1;
+    }
+
+    if (bind(fd, (struct sockaddr *) &addr, sizeof(struct sockaddr_un)) == -1) {
+        printf("Error binding to the address\n");
+        return -1;
+    }
+
+    // TODO listen
+
+    // TODO accept
+
+    if (close(fd) == -1) {
+        printf("Error closing the socket\n");
+        return -1;
+    }
 
     return 0;
 }
